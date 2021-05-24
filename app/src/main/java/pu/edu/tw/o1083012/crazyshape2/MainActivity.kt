3f9110e5aacc33b20b33f7cac470ab35fc08eea3
Student <o1083012@gm.pu.edu.tw>
2021-05-24 11:04:15 +0800

    0524

---
+++ app/src/main/java/pu/edu/tw/o1083012/crazyshape2/MainActivity.kt
@@ -0,0 +1,11 @@
+package pu.edu.tw.o1083012.crazyshape2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
